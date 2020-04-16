c2b1a7f9f62fac5cbfe18a9d474f65aefcb30830
Cristina <criss.ms7@gmail.com>
2020-04-15 21:51:05 -0500

    add zqq small signal to muoncr

--- fitting/ZbbJet/writeMuonCRDatacard.py
+++ fitting/ZbbJet/writeMuonCRDatacard.py
@@ -47,7 +47,7 @@ def writeDataCard(boxes,txtfileName,sigs,bkgs,histoDict,options):
         LUMI_ERR = 1.025
 
     #histToCard = {'tthqq125':'ttH_hbb','whqq125':'WH_hbb','hqq125':'ggH_hbb','zhqq125':'ZH_hbb','vbfhqq125':'qqH_hbb'}
-    histToCard = {}
+    histToCard = {'zqq':'zqq'}
     for bkg in bkgs:
         histToCard[bkg] = bkg
 
@@ -71,37 +71,34 @@ def writeDataCard(boxes,txtfileName,sigs,bkgs,histoDict,options):
         for box in boxes:
             error = array.array('d',[0.0])
             print('reading ',proc,box)
-            #qcd_pass_qcdpassmuonCR2016mcstat14Down
-            rate = histoDict['%s_%s'%(proc,box)].IntegralAndError(1,histoDict['%s_%s'%(proc,box)].GetNbinsX(),error)
-            effentries = histoDict['%s_%s'%(proc,box)].GetEffectiveEntries()
+            if 'zqq' in proc:
+                rate = 0.1
+                effentries = 0.1
+            else:
+                rate = histoDict['%s_%s'%(proc,box)].IntegralAndError(1,histoDict['%s_%s'%(proc,box)].GetNbinsX(),error)
+                effentries = histoDict['%s_%s'%(proc,box)].GetEffectiveEntries()
             rates['%s_%s'%(proc,box)]  = rate
             Effentries['%s_%s'%(proc,box)]  = effentries
             lumiErrs['%s_%s'%(proc,box)] = LUMI_ERR 
-            #lumiErrs['%s_%s'%(proc,box)] = 1.025
             if proc=='hqq125':
                 hqq125ptErrs['%s_%s'%(proc,box)] = 1.3                
             else:
                 hqq125ptErrs['%s_%s'%(proc,box)] = 1.0
             if proc=='wqq' or proc=='zqq' or 'hqq' in proc:
                 veffErrs['%s_%s'%(proc,box)] = 1.0+V_SF_ERR/V_SF
-                if box=='pass':
-                    bbeffErrs['%s_%s'%(proc,box)] = 1.0+BB_SF_ERR/BB_SF
-                else:
-                    ratePass = histoDict['%s_%s'%(proc,'pass')].Integral()
-                    rateFail = histoDict['%s_%s'%(proc,'fail')].Integral()
-                    if rateFail>0:
-                        bbeffErrs['%s_%s'%(proc,box)] = 1.0-BB_SF_ERR*(ratePass/rateFail)
-                    else:
-                        bbeffErrs['%s_%s'%(proc,box)] = 1.0
-                    
+                bbeffErrs['%s_%s'%(proc,box)] = 1.0
+                # if box=='pass':
+                #     bbeffErrs['%s_%s'%(proc,box)] = 1.0+BB_SF_ERR/BB_SF
+                # else:
+                #     ratePass = histoDict['%s_%s'%(proc,'pass')].Integral()
+                #     rateFail = histoDict['%s_%s'%(proc,'fail')].Integral()
+                #     if rateFail>0:
+                #         bbeffErrs['%s_%s'%(proc,box)] = 1.0-BB_SF_ERR*(ratePass/rateFail)
+                #     else:
+                #         bbeffErrs['%s_%s'%(proc,box)] = 1.0                    
             else:
                 veffErrs['%s_%s'%(proc,box)] = 1.
                 bbeffErrs['%s_%s'%(proc,box)] = 1.
-            #mutriggerErrs['%s_%s'%(proc,box)] = 1
-            #muidErrs['%s_%s'%(proc,box)] = 1
-            #muisoErrs['%s_%s'%(proc,box)] = 1
-            #jesErrs['%s_%s'%(proc,box)] = 1
-            #jerErrs['%s_%s'%(proc,box)] = 1
             if proc=='wqq':
                 wznormEWErrs['%s_%s'%(proc,box)] = 1.05
             else:
@@ -122,7 +119,7 @@ def writeDataCard(boxes,txtfileName,sigs,bkgs,histoDict,options):
                 for j in range(1,MASS_BINS+1):
                     mcStatErrs['%s_%s'%(proc,box),j] = 1.0
                 
-            if rate>0 and 'qcd' not in proc:
+            if rate>0 and 'qcd' not in proc and 'zqq' not in proc:
                 rateJESUp = histoDict['%s_%s_JESUp'%(proc,box)].Integral()
                 rateJESDown = histoDict['%s_%s_JESDown'%(proc,box)].Integral()
                 rateJERUp = histoDict['%s_%s_JERUp'%(proc,box)].Integral()
@@ -180,11 +177,6 @@ def writeDataCard(boxes,txtfileName,sigs,bkgs,histoDict,options):
     znormEWString = 'CMS_gghbb_znormEW\tlnN'
     znormQString = 'CMS_gghbb_znormQ\tlnN'    
     wznormEWString = 'CMS_gghbb_wznormEW\tlnN'
-    #muidString = 'muid\tshape'   
-    #muisoString = 'muiso\tshape'   
-    #mutriggerString = 'mutrigger\tshape'  
-    #jesString = 'JES\tshape'    
-    #jerString = 'JER\tshape'
     jesString = 'CMS_scale_j%s\tlnN'%options.suffix
     jerString = 'CMS_res_j%s\tlnN'%options.suffix
     puString = 'CMS_PU%s\tlnN'%options.suffix
@@ -212,11 +204,6 @@ def writeDataCard(boxes,txtfileName,sigs,bkgs,histoDict,options):
         for proc in sigs+bkgs:
             i+=1
             if rates['%s_%s'%(proc,box)] <= 0.0: continue
-            #if Effentries['%s_%s'%(proc,box)] <= 10.0:      
-            #    print "rejecting %s_%s , effective entries = %.3f <= 10,"%(proc,box,Effentries['%s_%s'%(proc,box)])
-            #    continue
-            #else:
-            #    print "acceptting %s_%s , effective entries = %.3f > 10,"%(proc,box,Effentries['%s_%s'%(proc,box)])
             binString +='\t%s_muonCR'%box
             processString += '\t%s'%(histToCard[proc])
             processNumberString += '\t%i'%(i-nSig+1)
@@ -269,9 +256,6 @@ def writeDataCard(boxes,txtfileName,sigs,bkgs,histoDict,options):
     for proc in (sigs+bkgs):
         for box in boxes:
             if rates['%s_%s'%(proc,box)] <= 0.0: continue
-            #if Effentries['%s_%s'%(proc,box)] <= 10.0:      
-            #    print "rejecting %s_%s , effective entries = %.3f <= 10,"%(proc,box,Effentries['%s_%s'%(proc,box)])
-            #    continue
             if options.noMcStatShape:                 
                 datacard+=mcStatErrString['%s_%s'%(proc,box)]
             else:
@@ -279,10 +263,8 @@ def writeDataCard(boxes,txtfileName,sigs,bkgs,histoDict,options):
                     error = histoDict['%s_%s'%(proc,box)].GetBinError(j)
                     rate  = histoDict['%s_%s'%(proc,box)].GetBinContent(j) 
                     if rate-error<=0:
-                        #print "rejecting %s_%s_%s"%(proc,box,j)
                         continue        ## veto small mcstat uncertainties
                     elif (error/rate)<=0.1 :
-                        #print "rejecting %s_%s_%s"%(proc,box,j)
                         continue        ## veto small mcstat uncertainties
                     else:
                         print "accepting %s_%s_%s with rate = %.5f, error = %.5f, error/rate = %.5f"%(proc,box,j,rate,error,error/rate)
@@ -298,7 +280,6 @@ def writeDataCard(boxes,txtfileName,sigs,bkgs,histoDict,options):
     #    'CMS_gghbb_tqqnormSF_%s extArg 1.0 [0.0,10.0]\n'%options.year + \
     #    'CMS_gghbb_tqqeffSF_%s extArg 1.0 [0.0,10.0]\n'%options.year
     
-    #datacard+='* autoMCStats 0 0 1\n'
     txtfile = open(options.odir+'/'+txtfileName,'w')
     txtfile.write(datacard)
     txtfile.close()
@@ -307,16 +288,8 @@ def writeDataCard(boxes,txtfileName,sigs,bkgs,histoDict,options):
 def main(options, args):
     
     boxes = ['pass', 'fail']
-    #for Zbb extraction:
-    sigs = [] 
-    bkgs = ['qcd','tqq','stqq'] # ,'zll']
-    #for Wqq/Zbb extraction:
-    #sigs = ['zqq','wqq']
-    #bkgs = ['tthqq125','whqq125','hqq125','zhqq125','vbfhqq125','qcd','tqq','vvqq','stqq','wlnu','zll']
-    #for just Zbb extraction:
-    #sigs = ['zqq']
-    #bkgs = ['tthqq125','whqq125','hqq125','zhqq125','vbfhqq125','qcd','tqq','wqq','vvqq','stqq','wlnu','zll']
-    #systs = ['JER','JES','mutrigger','muid','muiso','Pu']
+    sigs = ['zqq'] 
+    bkgs = ['qcd','tqq','stqq']
     systs = ['JER','JES','mutrigger','muid','muiso','Pu','mcstat']
 
     if options.year=='2018':
@@ -348,7 +321,7 @@ def main(options, args):
     fLoose = None
     removeUnmatched=False
     iPt=-1
-    for proc in (bkgs+sigs+['data_obs']):
+    for proc in (bkgs+['data_obs']):
         for box in boxes:
             print 'getting histogram for process: %s_%s'%(proc,box)
             histoDict['%s_%s'%(proc,box)] = tfile.Get('%s_%s'%(proc,box)).Clone()
@@ -360,11 +333,10 @@ def main(options, args):
                         print 'getting histogram for process: %s_%s_%sUp'%(proc,box,syst)
                         histoDict['%s_%s_%sUp'%(proc,box,syst)] = tfile.Get('%s_%s_%sUp'%(proc,box,syst)).Clone()
                         histoDict['%s_%s_%sUp'%(proc,box,syst)].Scale(GetSF(proc,box,tfile,fLoose,removeUnmatched,iPt,sf_dict))
-                        #print 'getting histogram for process: %s_%s_%sDown'%(proc,box,syst)
                         histoDict['%s_%s_%sDown'%(proc,box,syst)] = tfile.Get('%s_%s_%sDown'%(proc,box,syst)).Clone()
                         histoDict['%s_%s_%sDown'%(proc,box,syst)].Scale(GetSF(proc,box,tfile,fLoose,removeUnmatched,iPt,sf_dict))
                     else:
-                        #if proc=='qcd': continue
+                        if proc=='qcd': continue
                         histoDict['%s_%s_%s%smuonCR%smcstatUp'%(proc,box,proc,box,options.year)] = tfile.Get('%s_%s'%(proc,box)).Clone(tfile.Get('%s_%s'%(proc,box)).GetName()+'_%s%smuonCR%smcstatUp'%(proc,box,options.year))
                         histoDict['%s_%s_%s%smuonCR%smcstatUp'%(proc,box,proc,box,options.year)].Scale(GetSF(proc,box,tfile,fLoose,removeUnmatched,iPt,sf_dict))
                         histoDict['%s_%s_%s%smuonCR%smcstatDown'%(proc,box,proc,box,options.year)] = tfile.Get('%s_%s'%(proc,box)).Clone(tfile.Get('%s_%s'%(proc,box)).GetName()+'_%s%smuonCR%smcstatDown'%(proc,box,options.year))
@@ -374,16 +346,34 @@ def main(options, args):
                                 histoDict['%s_%s'%(proc,box)].SetBinContent(ibin,0) 
                             histoDict['%s_%s_%s%smuonCR%smcstatUp'%(proc,box,proc,box,options.year)].SetBinContent(ibin, histoDict['%s_%s'%(proc,box)].GetBinContent(ibin)+ histoDict['%s_%s'%(proc,box)].GetBinError(ibin)) 
                             histoDict['%s_%s_%s%smuonCR%smcstatDown'%(proc,box,proc,box,options.year)].SetBinContent(ibin,max(1E-06, histoDict['%s_%s'%(proc,box)].GetBinContent(ibin)- histoDict['%s_%s'%(proc,box)].GetBinError(ibin))) 
-    uncorrelate(histoDict, 'mcstat')
 
+    def ZeroHistogram1D(h):
+        for i in range(1, h.GetNbinsX() + 1):
+            h.SetBinContent(i, 0.01)
+            h.SetBinError(i, 0.)
+
+    for proc in (sigs):
+        for box in boxes:
+            print 'getting histogram for process: %s_%s'%(proc,box)
+            h = tfile.Get('stqq_%s'%(box)).Clone('%s_%s'%(proc,box))
+            h.SetDirectory(0)
+            ZeroHistogram1D(h)
+            histoDict['%s_%s'%(proc,box)] = h
+            for syst in systs:
+                if syst!='mcstat':
+                    histoDict['%s_%s_%sUp'%(proc,box,syst)] = h.Clone('%s_%s_%sUp'%(proc,box,syst))
+                    histoDict['%s_%s_%sDown'%(proc,box,syst)] = h.Clone('%s_%s_%sDown'%(proc,box,syst))
+                else:
+                    histoDict['%s_%s_%s%smuonCR%smcstatUp'%(proc,box,proc,box,options.year)] = h.Clone('%s_%s_%s%smuonCR%smcstatUp'%(proc,box,proc,box,options.year))
+                    histoDict['%s_%s_%s%smuonCR%smcstatDown'%(proc,box,proc,box,options.year)] = h.Clone('%s_%s_%s%smuonCR%smcstatDown'%(proc,box,proc,box,options.year))
+
+    uncorrelate(histoDict, 'mcstat')
     
     outFile = 'datacard_muonCR.root'
     outFileHist = 'datacard_muonCR_hist.root'
     
     outputFile = rt.TFile.Open(options.odir+'/'+outFile,'recreate')
     w = rt.RooWorkspace('w_muonCR')
-    #w.factory('y[40,40,201]')
-    #w.var('y').setBins(1)
     w.factory('x[%i,%i,%i]'%(MASS_LO,MASS_LO,MASS_HI))
     w.var('x').setBins(MASS_BINS)
 
@@ -391,8 +381,6 @@ def main(options, args):
     for key, histo in histoDict.iteritems():
         outputFileHist.cd()
         histo.Write(key)
-        #histo.Rebin(23)
-        #ds = rt.RooDataHist(key,key,rt.RooArgList(w.var('y')),histo)
         ds = rt.RooDataHist(key,key,rt.RooArgList(w.var('x')),histo)
         getattr(w,'import')(ds, rt.RooCmdArg())
     outputFileHist.Close()
@@ -408,7 +396,7 @@ def main(options, args):
     print(histoDict['qcd_pass'].IntegralAndError(1,histoDict['qcd_pass'].GetNbinsX(),array.array('d',[0.0])))
     writeDataCard(boxes,txtfileName,sigs,bkgs,histoDict,options)
     print '\ndatacard:\n'
-    print(txtfileName)
+    print('%s/%s'%(options.odir,txtfileName))
     os.system('cat %s/%s'%(options.odir,txtfileName))
 
 
