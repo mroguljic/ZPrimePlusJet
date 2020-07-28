135844e1149cd88e90d2ee0a0b4ce88d681eca31
Cristina <criss.ms7@gmail.com>
2020-07-28 07:52:27 -0500

    add higher order poly

--- fitting/ZbbJet/buildRhalphabetZbb.py
+++ fitting/ZbbJet/buildRhalphabetZbb.py
@@ -25,31 +25,54 @@ RHO_LO = -6
 RHO_HI = -2.1
 
 SF2018={
-            'shift_SF'  : 0.970,           'shift_SF_ERR' : 0.012,
-            'smear_SF'  : 0.9076,          'smear_SF_ERR' : 0.0146,
-            'V_SF'      : 0.953,           'V_SF_ERR'     : 0.016,  
-            'BB_SF'     : 1.0,             'BB_SF_ERR' : 0.3,     ## M2 SF
+    'shift_SF'  : 0.970,           'shift_SF_ERR' : 0.012,
+    'smear_SF'  : 0.9076,          'smear_SF_ERR' : 0.0146,
+    'V_SF'      : 0.953,           'V_SF_ERR'     : 0.016,
+    'BB_SF'     : 1.0,             'BB_SF_ERR' : 0.3,     ## M2 SF                                                                                                        
 }
 SF2017={
-            'shift_SF'  : 0.978,           'shift_SF_ERR' : 0.012,
-            'smear_SF'  : 0.9045,          'smear_SF_ERR' : 0.048,
-            'V_SF'      : 0.924,           'V_SF_ERR'  : 0.018,  
-            'BB_SF'     : 1.0,             'BB_SF_ERR' : 0.3        , # prelim ddb SF
+    'shift_SF'  : 0.978,           'shift_SF_ERR' : 0.012,
+    'smear_SF'  : 0.9045,          'smear_SF_ERR' : 0.048,
+    'V_SF'      : 0.924,           'V_SF_ERR'  : 0.018,
+    'BB_SF'     : 1.0,             'BB_SF_ERR' : 0.3        , # prelim ddb SF                                                                                              
 }
 SF2016={
-            'BB_SF'     : 1.0,             'BB_SF_ERR' : 0.23,     ## M2 SF
-            'V_SF'      : 0.993,            'V_SF_ERR'  : 0.043,
-            #'shift_SF'  : 1.014,             'shift_SF_ERR': 0.006, # tau21ddt 0.43
-            #'smear_SF'  : 1.086,             'smear_SF_ERR': 0.086, # tau21ddt 0.43
-            'shift_SF'  : 1.001,            'shift_SF_ERR' : 0.012   , # m_data/m_mc, sqrt((m_data_err/m_data)**2+(m_mc_err/m_mc)**2)                                           
-            'smear_SF'  : 1.084,            'smear_SF_ERR' : 0.0905  ,
-        }
+    'BB_SF'     : 1.0,             'BB_SF_ERR' : 0.23,     ## M2 SF
+    'V_SF'      : 0.993,            'V_SF_ERR'  : 0.043,
+    'shift_SF'  : 1.001,            'shift_SF_ERR' : 0.012   , # m_data/m_mc, sqrt((m_data_err/m_data)**2+(m_mc_err/m_mc)**2)                                           
+    'smear_SF'  : 1.084,            'smear_SF_ERR' : 0.0905  ,
+}
+
+qcdTFpars_2018 ={'n_rho':2, 'n_pT':2,
+                 'pars':[0.0139,-0.9680,2.3695 ,0.6775 ,1.0759 ,1.4427 ,0.1826 ,0.2077 ,1.8612 ,-0.8737],
+                 'fitpath':'ddb2018_Jun24_v3/ddb_M2_full/TF22_MC_w2Fit/rhalphabase.root'}
+qcdTFpars_2017={'n_rho':2, 'n_pT':2,
+                'pars':[ 0.0151 , -1.0359, 2.3953 , 0.7093 , 1.0947 , 1.6930 , -0.1745, 0.1980 , 1.4567 , -0.0427],
+                'fitpath':'ddb_Jun24_v2/ddb_M2_full/TF22_MC_w2Fit/rhalphabase.root'}
+qcdTFpars_2016_dak8 ={#'n_rho':3,'n_pT':5, 
+                      #'pars':[0.0201,
+                      #        4.9506, -0.5974, 1.7446, 0.7833, -2.826, -1.3115, 6.0557, -1.7539,  2.2512, 
+                      #        13.25453, -11.7266, 5.429, 7.832, -29.377, 27.9622, -15.8643, 0.27883, 29.89604, -11.7782, 14.5337],
+                      #'fitpath':'dak8/dak8_M2_2016_May10_tightMatch_ttvetoM_withDDT/TF35_MC_w2Fitv2/rhalphabase.root',
+                      'n_rho':4, 'n_pT':3,
+                      'pars':[0.02012216832938594, -2.1245752517206924, 4.642350520743385, -1.0952863593351232, 2.125819045977998, 0.6400817635219482, 18.09067598406464, -13.524206910672952, 8.159227666670368, 1.5938158455076206, -1.5578556549905969, -21.721069806832972, 13.618575996538375, 2.7475908117196326, -8.303068594227735, 4.067853838371654, 26.164655421043356, -8.649124391553201, -6.678034076297433, 19.255508863004238, -17.797774928138846],
+                      'fitpath':'dak8/dak8_M2_2016_May10_tightMatch_ttvetoM_withDDT/TF43_MC_w2Fitv2/rhalphabase.root',
+                      #'n_rho':3, 'n_pT':4,
+                      #'pars':[0.02012216832938594, 4.882609080643348, -0.7825180500719675, 1.9452746069951488, 0.699752258725546, -6.188988214828072, 2.041255428456303, 4.2384682149208075, -1.2307220499218658, 15.699706656941188, 1.3643528905941835, -6.996883061214927, 2.946363828315384, -20.28729986279143, -7.770939022211714, 26.481927721827127, -13.404608598818896, 29.76224377560486, 14.174444785751547, -29.99996571889061, 5.807858832284083],
+                      #'fitpath':'dak8/dak8_M2_2016_May10_tightMatch_ttvetoM_withDDT/TF34_MC_w2Fitv2/rhalphabase.root',
+                      #'n_rho':2, 'n_pT':2,
+                      #'pars':[0.020122168239502677, 0.38112978719457047, 1.8493237587961104, 0.7285856838418248, -0.4517927327288014, 3.779697562244934, -1.393937860245007\
+                      #        , 3.0742854675930147, 0.15761645878317054, -1.6113936238170368],
+                      #'fitpath':'dak8/dak8_M2_2016_May10_tightMatch_ttvetoM_withDDT/TF22_MC_w2Fitv2/rhalphabase.root'
+                      }
+qcdTFpars_2016_ddb = {'n_rho':2, 'n_pT':2,
+                      'pars':[ 0.012287,-1.0841,2.0706, 0.82500,1.2746 ,1.8651 ,0.24751,0.43354 ,1.2328 ,-0.00224],
+                      'fitpath':'ddb/ddb_M2_2016/TF22_MC_w2Fitv2/rhalphabase.root'}
 
 def main(options, args):
     ifile = options.ifile
     odir = options.odir
 
-    print "loading default rhalphabet_builder"
     from rhalphabet_builder import RhalphabetBuilder, LoadHistograms, GetSF
     # Load the input histograms
     # 	- 2D histograms of pass and fail mass,pT distributions
@@ -61,45 +84,23 @@ def main(options, args):
         fLoose = r.TFile.Open(options.ifile_loose)
     if   options.year =='2018':
           sf=SF2018
-          if not options.pseudo:
-              # qcd TF pars for ddb fit (hbb)    
-              qcdTFpars ={'n_rho':2, 'n_pT':2,
-                          'pars':[0.0139,-0.9680,2.3695 ,0.6775 ,1.0759 ,1.4427 ,0.1826 ,0.2077 ,1.8612 ,-0.8737],
-                          'fitpath':'ddb2018_Jun24_v3/ddb_M2_full/TF22_MC_w2Fit/rhalphabase.root'}
+          if not options.pseudo:    qcdTFpars = qcdTFpars_2018 
     elif options.year =='2017':
           sf=SF2017
-          if not options.pseudo: 
-              # qcd TF pars for ddb fit (hbb) 
-              qcdTFpars = {'n_rho':2, 'n_pT':2,
-                           'pars':[ 0.0151 , -1.0359, 2.3953 , 0.7093 , 1.0947 , 1.6930 , -0.1745, 0.1980 , 1.4567 , -0.0427],
-                           'fitpath':'ddb_Jun24_v2/ddb_M2_full/TF22_MC_w2Fit/rhalphabase.root'}
-
+          if not options.pseudo:    qcdTFpars = qcdTFpars_2017
     elif options.year =='2016':
           sf=SF2016
           if not options.pseudo:  
               if 'dak8' in ifile: # dak8
-                  #qcdTFpars ={'n_rho':2, 'n_pT':2,
-                  #            'pars':[0.0053,-0.6291,1.7345,0.8998,0.9977,2.0318,0.4615,0.8688,1.0347,-0.3260],
-                  #            'fitpath':'dak8/dak8_M2_2016/TF22_MC_w2Fitv2/rhalphabase.root'}
-                  qcdTFpars ={'n_rho':2, 'n_pT':2,                                                                                                                                
-                              #'pars':[1.7914,0.0060,1.4139,-0.6543,2.0425,1.3397,2.5851,0.5730,0.7028],
-                              ##'pars':[ 0.01270,  1.7807, 0.0127, 1.4103,-0.6054, 2.00438, 1.3546, 2.47630, 0.691304, 0.58238],
-                              ##'fitpath':'dak8/dak8_M2_2016_Feb26_tightMatch_ttvetoT_withDDT/TF22_MC_w2Fitv2/rhalphabase.root'} 
-                              'pars':[0.020122168239502677, 0.38112978719457047, 1.8493237587961104, 0.7285856838418248, -0.4517927327288014, 3.779697562244934, -1.393937860245007, 3.0742854675930147, 0.15761645878317054, -1.6113936238170368],
-                              'fitpath':'dak8/dak8_M2_2016_Apr10_tightMatch_ttvetoT_withDDT/TF22_MC_w2Fitv2/rhalphabase.root'}
+                  qcdTFpars = qcdTFpars_2016_dak8
               else: # ddb
-                  qcdTFpars = {'n_rho':2, 'n_pT':2,
-                               'pars':[ 0.012287,-1.0841,2.0706, 0.82500,1.2746 ,1.8651 ,0.24751,0.43354 ,1.2328 ,-0.00224],
-                               'fitpath':'ddb/ddb_M2_2016/TF22_MC_w2Fitv2/rhalphabase.root'}
-
+                  qcdTFpars = qcdTFpars_2016_ddb
 
     (pass_hists,fail_hists) = LoadHistograms(f, options.pseudo, options.blind, options.useQCD, scale=options.scale, r_signal=options.r, mass_range=[MASS_HIST_LO, MASS_HIST_HI], blind_range=[BLIND_LO, BLIND_HI], rho_range=[RHO_LO,RHO_HI], fLoose=fLoose,sf_dict=sf,createPassFromFail=options.createPassFromFail,skipQCD=options.skipQCD)
 
     # Build the workspacees
     rhalphabuilder = RhalphabetBuilder(pass_hists, fail_hists, f, options.odir, nr=options.NR, np=options.NP, mass_nbins=MASS_BINS, mass_lo=MASS_LO, mass_hi=MASS_HI, blind_lo=BLIND_LO, blind_hi=BLIND_HI, rho_lo=RHO_LO, rho_hi=RHO_HI, blind=options.blind, mass_fit=options.massfit, freeze_poly=options.freeze, remove_unmatched=options.removeUnmatched, input_file_loose=fLoose,suffix=options.suffix,sf_dict=sf,mass_hist_lo=MASS_HIST_LO,mass_hist_hi=MASS_HIST_HI,qcdTFpars=qcdTFpars,exp=options.exp,multi=options.multi,pseudo=options.pseudo)
     rhalphabuilder.run()
-    if options.addHptShape:
-        rhalphabuilder.addHptShape()	
     if options.prefit:
         rhalphabuilder.prefit()
     elif options.loadfit is not None:
@@ -137,7 +138,6 @@ if __name__ == '__main__':
     parser.add_option('-r', dest='r', default=1, type='float', help='signal strength for MC pseudodataset')
     parser.add_option('--remove-unmatched', action='store_true', dest='removeUnmatched', default =False,help='remove unmatched', metavar='removeUnmatched')
     parser.add_option('--prefit', action='store_true', dest='prefit', default =False,help='do prefit', metavar='prefit')
-    parser.add_option('--addHptShape',action='store_true',dest='addHptShape',default =False,help='add H pt shape unc', metavar='addHptShape')
     parser.add_option('--loadfit', dest='loadfit', default=None, help='load qcd polynomial parameters from alternative rhalphabase.root',metavar='loadfit')
     parser.add_option('-y' ,'--year', type='choice', dest='year', default ='2016',choices=['2016','2017','2018'],help='switch to use different year ', metavar='year')
     parser.add_option('--suffix', dest='suffix', default='', help='suffix for conflict variables',metavar='suffix')
